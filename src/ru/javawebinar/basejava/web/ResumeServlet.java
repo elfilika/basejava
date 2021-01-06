015d93a331e093443b6bf8a97670997e9bbaa6bc
elfilika <elfilika@gmail.com>
2021-01-06 18:40:50 +0700

    Lesson 15: tomcat+servlet

---
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.basejava.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        //response.getWriter().write("Hello Resumes!");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
